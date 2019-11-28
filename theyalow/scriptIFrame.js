58f935cdd9c73007f8d345dcf0f30256f75000ae
Katsiaryna31 <katia310790@mail.ru>
2019-11-28 20:44:53 +0300

    feat: add iframe folders 2

---
+++ theyalow/scriptIFrame.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const desktopButton = document.querySelector('.desktop-button');
+const mobileButton = document.querySelector('.mobile-button');
+const iframe = document.getElementById('iframe');
+const container = document.querySelector('.container');
+container.style.width = '100%';
+iframe.style.width = '100%';
+iframe.style.height = '1850px';
+
+const changeVersion = () => {
+    if (desktopButton.classList.contains('version--active')) {
+        desktopButton.classList.remove('version--active');
+        mobileButton.classList.add('version--active');
+        iframe.style.height = '1850px';
+        container.style.width = '100%';
+        container.style.margin = '0 auto';
+    } else {
+        desktopButton.classList.add('version--active');
+        mobileButton.classList.remove('version--active');
+        iframe.style.height = '1514px';
+        container.style.width = '640px';
+        container.style.margin = '0 auto';
+    }
+}
+
+desktopButton.addEventListener('click', changeVersion);
+mobileButton.addEventListener('click', changeVersion);
+
+if (window.screen.width <= 1440) {
+    desktopButton.classList.remove('version--active');
+    mobileButton.classList.remove('version--active');
+    container.style.width === window.screen.width;
+}
